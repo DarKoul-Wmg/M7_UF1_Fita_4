0edc15e0dbc5e7aa48f17867d9f7a01e7616407f
DarKoul-Wmg <williammg0904@gmail.com>
2024-10-03 22:11:31 +0200

    ex2 acabado

--- ex_4_2/ex42pagina2.php
+++ ex_4_2/ex42pagina2.php
@@ -1,12 +1,10 @@
-<p>ENREGISTRA FRASE 2</p>
-
 <?php  
 	session_start();
-	$_SESSION['ocult'] = $_POST['frase1'];
+	$_SESSION['frase1'] = $_POST['frase1'];
+?>
+<p>ENREGISTRA FRASE 2</p>
 
-	echo'<form method="post" action="ex42pagina3.php">
+<form method="post" action="ex42pagina3.php">
 	<input type="text" name="frase2">
 	<input type="submit" value="Envia">
-	</form>';
-?>
-
+</form>
--- ex_4_2/ex42pagina3.php
+++ ex_4_2/ex42pagina3.php
@@ -1,23 +1,43 @@
-<p>COINCIDENCIES<p>
-
- <a href="ex42pagina1.php">Pagina 1</a>
- <br/>
-
 <?php 
-	session_start();
-	$_SESSION['frase2'] = $_POST['frase2'];
+session_start();
 
-	$frase1 = explode(" ",$_SESSION['frase1']);
-	$frase2 = explode(" ",$_SESSION['frase2']);
-	$contador = 0;
+if (isset($_POST['frase2'])) {
+    $_SESSION['frase2'] = $_POST['frase2'];
+}
 
-	foreach ($frase1 as $word) {
-    if (in_array($word, $frase2) !== false) {
+if (isset($_SESSION['frase1'])) {
+    $frase1 = $_SESSION['frase1'];
+} else {
+    $frase1 = '';
+}
+
+if (isset($_SESSION['frase2'])) {
+    $frase2 = $_SESSION['frase2'];
+} else {
+    $frase2 = '';
+}
+
+$frase1_array = explode(" ", $frase1);
+$frase2_array = explode(" ", $frase2);
+$contador = 0;
+
+?>
+
+<p>COINCIDENCIES</p>
+
+<a href="ex42pagina1.php">Pagina 1</a>
+<br/>
+
+<?php
+// Comparar las palabras de ambas frases
+foreach ($frase1_array as $word) {
+    if (in_array($word, $frase2_array)) {
         echo "<p>- La paraula '$word' està a les dues frases.</p>";
-        $contador +=1;
-    	}
-	}
-	if($contador == 0){
-		echo "<p>No hi ha cap coincidència.</p>";
-	}
+        $contador++;
+    }
+}
+
+if ($contador == 0) {
+    echo "<p>No hi ha cap coincidència.</p>";
+}
 ?>
